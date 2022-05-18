94b8b27556cce2e6acf3b0aa54cac27fd39ae45b
Michal Lappo <michal.lappo@gmail.com>
2022-05-18 12:00:23 +0200

    feat: disk data upload

--- cmd/meana-agent/main.go
+++ cmd/meana-agent/main.go
@@ -1,11 +1,82 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/meana-io/meana-agent/pkg/disk"
 )
+
+const AgentInterval = 5 * time.Second
+
+type AgentData struct {
+	Disk *disk.DiskData `json:"disk"`
+}
+
+func ValidateEnv() error {
+	if os.Getenv("MEANA_SERVER_ADDR") == "" {
+		return fmt.Errorf("meana server address not specified")
+	}
+
+	return nil
+}
+
+func CollectData() (*AgentData, error) {
+	var data AgentData
+	diskData, err := disk.Data()
+
+	if err != nil {
+		return nil, err
+	}
+
+	data.Disk = diskData
+
+	return &data, nil
+}
+
+func UploadData(data *AgentData) error {
+	c := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+
+	postBody, _ := json.Marshal(data)
+
+	responseBody := bytes.NewBuffer(postBody)
+
+	resp, err := c.Post(os.Getenv("MEANA_SERVER_ADDR")+"/tmp", "application/json", responseBody)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func HandleAgentError(err error) {
+	log.Printf("Error: %v\n", err)
+}
+
+func AgentRoutine() {
+	data, err := CollectData()
+	if err != nil {
+		HandleAgentError(fmt.Errorf("error collecting agent data: %v", err))
+		return
+	}
+
+	err = UploadData(data)
+	if err != nil {
+		HandleAgentError(fmt.Errorf("error uploading agent data: %v", err))
+		return
+	}
+}
 
 func main() {
 	err := godotenv.Load()
@@ -14,5 +85,14 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	disk.Data()
+	err = ValidateEnv()
+
+	if err != nil {
+		log.Fatalf("Error validating .env: %v", err)
+	}
+
+	for {
+		go AgentRoutine()
+		time.Sleep(AgentInterval)
+	}
 }
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29 // indirect
 	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	howett.net/plist v1.0.0 // indirect
