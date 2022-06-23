f10e1929bc5eef9e8ee1bdcd5f84789ceaf61144
Michał Łappo <michal.lappo@gmail.com>
2022-06-23 20:54:02 +0200

    feat: dmidecode parse

--- pkg/ram/ram.go
+++ pkg/ram/ram.go
@@ -1,9 +1,11 @@
 package ram
 
 import (
+	"fmt"
 	"strconv"
 
-	"github.com/pbnjay/memory"
+	dmidecode "github.com/dselans/dmidecode"
+	mem "github.com/pbnjay/memory"
 )
 
 type RamData struct {
@@ -14,8 +16,30 @@ type RamData struct {
 func GetRamData() (*RamData, error) {
 	var data RamData
 
-	total := memory.TotalMemory()
-	free := memory.FreeMemory()
+	dmi := dmidecode.New()
+
+	if err := dmi.Run(); err != nil {
+		fmt.Printf("Unable to get dmidecode information. Error: %v\n", err)
+	}
+
+	// // You can search by record name
+	// byNameData, _ := dmi.SearchByName("System Information")
+
+	// // or you can also search by record type
+	// byTypeData, _ := dmi.SearchByType(1)
+
+	// fmt.Printf(string(json.Marshal(byNameData)))
+
+	// or you can just access the data directly
+	// for handle, record := range dmi.Data {
+	// 	fmt.Println("Checking record:", handle)
+	// 	for k, v := range record {
+	// 		fmt.Printf("Key: %v Val: %v\n", k, v)
+	// 	}
+	// }
+
+	total := mem.TotalMemory()
+	free := mem.FreeMemory()
 
 	data.Total = strconv.FormatUint(total, 10)
 	data.Used = strconv.FormatUint(total-free, 10)
