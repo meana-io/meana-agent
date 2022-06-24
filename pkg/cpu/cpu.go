e1e9c6183f25bdde9e3948e55af0f0b95f79a46a
Michał Łappo <michal.lappo@gmail.com>
2022-06-24 23:17:20 +0200

    fix: cpu usage from deltas

--- pkg/cpu/cpu.go
+++ pkg/cpu/cpu.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	linuxproc "github.com/c9s/goprocinfo/linux"
 	dmidecode "github.com/dselans/dmidecode"
@@ -16,6 +17,8 @@ type CpuData struct {
 	Model         string `json:"model"`
 	Usage         string `json:"usage"`
 }
+
+const CpuLoadInterval = 50 * time.Millisecond
 
 func GetCpuData() (*CpuData, error) {
 	var data CpuData
@@ -36,18 +39,54 @@ func GetCpuData() (*CpuData, error) {
 		data.Model = byTypeData[0]["Family"]
 	}
 
-	stat, err := linuxproc.ReadStat("/proc/stat")
+	usage, err := calculateCpuUsage()
+
 	if err != nil {
-		log.Printf("Error getting linuxproc info: %v", err)
+		log.Printf("Error getting cpu usage info: %v", err)
 		return nil, err
 	}
 
-	var usage uint64
-	usage = stat.CPUStatAll.User + stat.CPUStatAll.Guest + stat.CPUStatAll.GuestNice + stat.CPUStatAll.IOWait + stat.CPUStatAll.IRQ + stat.CPUStatAll.Nice + stat.CPUStatAll.SoftIRQ + stat.CPUStatAll.Steal + stat.CPUStatAll.System
-	var percent float64
-	percent = float64(usage) / float64(usage+stat.CPUStatAll.Idle)
+	data.Usage = strconv.FormatFloat(usage, 'f', 10, 64)
 
-	data.Usage = strconv.FormatFloat(percent, 'f', 10, 64)
+	log.Printf("%v", data.Usage)
 
 	return &data, nil
+}
+
+func calculateCpuUsage() (float64, error) {
+	stat, err := linuxproc.ReadStat("/proc/stat")
+	if err != nil {
+		log.Printf("Error getting linuxproc info: %v", err)
+		return 0, err
+	}
+
+	var total uint64
+	var work uint64
+	work = stat.CPUStatAll.User + stat.CPUStatAll.Nice + stat.CPUStatAll.System + stat.CPUStatAll.IOWait + stat.CPUStatAll.IRQ + stat.CPUStatAll.SoftIRQ
+	total = work + stat.CPUStatAll.Idle
+
+	time.Sleep(CpuLoadInterval)
+
+	stat2, err := linuxproc.ReadStat("/proc/stat")
+	if err != nil {
+		log.Printf("Error getting linuxproc info: %v", err)
+		return 0, err
+	}
+
+	var total2 uint64
+	var work2 uint64
+	work2 = stat2.CPUStatAll.User + stat2.CPUStatAll.Nice + stat2.CPUStatAll.System + stat2.CPUStatAll.IOWait + stat2.CPUStatAll.IRQ + stat2.CPUStatAll.SoftIRQ
+	total2 = work2 + stat2.CPUStatAll.Idle
+
+	totalOver := total2 - total
+	workOver := work2 - work
+
+	log.Printf("%v", totalOver)
+	log.Printf("%v", workOver)
+
+	var percent float64
+
+	percent = float64(workOver) / float64(totalOver) * 100
+
+	return percent, nil
 }
