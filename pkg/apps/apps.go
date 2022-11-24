f726d029df83af96c49ce4f64226a00db190435f
Michał Łappo <michal.lappo@gmail.com>
2022-11-24 20:31:52 +0100

    fix: apps upgradable

--- pkg/apps/apps.go
+++ pkg/apps/apps.go
@@ -7,13 +7,18 @@ import (
 )
 
 type AppsData struct {
-	Apps map[string]string `json:"apps"`
+	Apps map[string]App `json:"apps"`
+}
+
+type App struct {
+	Version    string `json:"version"`
+	Upgradable bool   `json:"upgradable"`
 }
 
 func GetAppsData() (*AppsData, error) {
 	var appsData AppsData
 
-	appsData.Apps = make(map[string]string)
+	appsData.Apps = make(map[string]App)
 
 	output, err := exec.Command(
 		"apt",
@@ -29,7 +34,10 @@ func GetAppsData() (*AppsData, error) {
 	for _, app := range strings.Split(string((output)), "\n") {
 		split := strings.Split(app, " ")
 		if len(split) > 1 {
-			appsData.Apps[strings.Split(split[0], "/")[0]] = split[1]
+			var app App
+			app.Version = split[1]
+			app.Upgradable = strings.Contains(split[3], "upgradable")
+			appsData.Apps[strings.Split(split[0], "/")[0]] = app
 		}
 	}
 
